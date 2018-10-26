b8dddbf7c5af780c5633103f9d5a2616bb43e4f0
Ivan Harasta <harasta.ivan@gmail.com>
2018-10-26 09:11:59 +0200

    Create Customer class

---
+++ Customer.php
@@ -0,0 +1,57 @@
+<?php
+
+class Customer
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Customer constructor.
+     * @param int $id
+     * @param string $name
+     */
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
