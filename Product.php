91b11d688ab6b6c32c31d923f1957e89aa298de2
Ivan Harasta <harasta.ivan@gmail.com>
2018-10-26 09:09:28 +0200

    Create Product class

---
+++ Product.php
@@ -0,0 +1,80 @@
+<?php
+
+class Product
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var float
+     */
+    private $price;
+
+    /**
+     * Product constructor.
+     * @param int $id
+     * @param string $name
+     * @param float $price
+     */
+    public function __construct($id, $name, $price)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+}
