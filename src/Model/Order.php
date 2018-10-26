f333d535b4a534f84912b61d6cb1ce8719253096
Ivan Harašta <harasiva@fit.cvut.cz>
2018-10-26 11:38:16 +0200

    Move classes to Model/

--- src/Model/Order.php
+++ src/Model/Order.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace EShop;
+namespace EShop\Model;
 
 class Order
 {
--- tests/test.php
+++ tests/test.php
@@ -4,24 +4,24 @@ include_once __DIR__ . '/../autoload.php';
 
 $now = \DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s'));
 
-$customer1 = new EShop\Customer(1, "Testovaci 1");
-$customer2 = new EShop\Customer(2, "Testovaci 2");
-$customer3 = new EShop\Customer(3, "Testovaci 3");
-$customer4 = new EShop\Customer(4, "Testovaci 4");
-$customer5 = new EShop\Customer(5, "Testovaci 5");
+$customer1 = new EShop\Model\Customer(1, "Testovaci 1");
+$customer2 = new EShop\Model\Customer(2, "Testovaci 2");
+$customer3 = new EShop\Model\Customer(3, "Testovaci 3");
+$customer4 = new EShop\Model\Customer(4, "Testovaci 4");
+$customer5 = new EShop\Model\Customer(5, "Testovaci 5");
 
-$product1 = new EShop\Product(1, 'Product 1', 12.5);
-$product2 = new EShop\Product(2, 'Product 2', 12.5);
-$product3 = new EShop\Product(3, 'Product 3', 12.5);
-$product4 = new EShop\Product(4, 'Product 4', 12.5);
-$product5 = new EShop\Product(5, 'Product 5', 12.5);
+$product1 = new EShop\Model\Product(1, 'Product 1', 12.5);
+$product2 = new EShop\Model\Product(2, 'Product 2', 12.5);
+$product3 = new EShop\Model\Product(3, 'Product 3', 12.5);
+$product4 = new EShop\Model\Product(4, 'Product 4', 12.5);
+$product5 = new EShop\Model\Product(5, 'Product 5', 12.5);
 
-$order1 = new EShop\Order(1, $now, $now, $customer1, [
+$order1 = new EShop\Model\Order(1, $now, $now, $customer1, [
     $product1,
     $product3
 ]);
 
-$order2 = new EShop\Order(2, $now, $now, $customer4, [
+$order2 = new EShop\Model\Order(2, $now, $now, $customer4, [
     $product2,
     $product3,
     $product5
