f93d6445983e7fe26e3c160de580f4611e581e69
Ivan Harašta <harasiva@fit.cvut.cz>
2018-10-26 11:27:06 +0200

    Create basic tests

--- autoload.php
+++ autoload.php
@@ -3,5 +3,5 @@
 spl_autoload_register(function ($className) {
     $class = str_replace('\\', '/', $className);
     $path = str_replace('EShop', 'src', $class);
-    include_once($path . '.php');
+    include_once(__DIR__ . '/' . $path . '.php');
 });
